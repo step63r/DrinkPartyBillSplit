bdb2ccbf16b785c4f0176e3d0c6b42d2f3460498
Ryosuke UCHIYAMA <uchi63rs@yahoo.co.jp>
2019-07-05 22:03:26 +0900

    プロジェクト ファイルを追加します。

---
+++ DrinkPartyBillSplit/DrinkPartyBillSplit/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using DrinkPartyBillSplit.Models;
+
+namespace DrinkPartyBillSplit.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
